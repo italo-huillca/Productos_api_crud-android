975f50c4c2800be94b31a51a665fa64427cbf9e9
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-27 15:21:25 -0500

    feat: Reemplazar la implementación de la pantalla principal para utilizar NavGraph

--- app/src/main/java/com/huillca/productos_api_crud/MainActivity.kt
+++ app/src/main/java/com/huillca/productos_api_crud/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.huillca.productos_api_crud.navigation.NavGraph
 import com.huillca.productos_api_crud.ui.theme.Productos_api_crudTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Productos_api_crudTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ProductosApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ProductosApp() {
+    val navController = rememberNavController()
+    NavGraph(navController = navController)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ProductosAppPreview() {
     Productos_api_crudTheme {
-        Greeting("Android")
+        ProductosApp()
     }
 }
